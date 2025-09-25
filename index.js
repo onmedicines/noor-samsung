a94e96138c241c296486beee1384b7e99cd45f98
anurag-mac <semwalanurag26@gmail.com>
2025-09-25 15:35:19 +0530

    Solved- probably

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/health", () => console.log("hello"));
+app.get("/api/health", () => res.json({ hello: "world" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/complaints", complaintRoutes);
 
