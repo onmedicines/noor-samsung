18ca914cf26ebcb0c35cef8e6f556f0e084e4abf
anurag-mac <semwalanurag26@gmail.com>
2025-09-25 15:29:09 +0530

    Add a health route

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/health", () => console.log("hello"));
 app.use("/api/auth", authRoutes);
 app.use("/api/complaints", complaintRoutes);
 
